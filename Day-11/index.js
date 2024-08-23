918a7ce1a0f3324dfc485d67a954e366da31ff12
pratibhasingh47 <pratibhasingh1624@gmail.com>
2024-08-23 11:55:24 +0530

    js

---
+++ Day-11/index.js
@@ -0,0 +1,35 @@
+// String Concatenation:
+// Question 1: String Concatenation
+function getFullName(firstName, lastName) {
+    const fullName = firstName + " " + lastName;
+    console.log(fullName);
+}
+
+// Example usage:
+getFullName("John", "Doe");
+
+
+// If-Else Statements:
+// Question 2: If-Else Statements
+function checkNumber(num) {
+    if (num > 0) {
+        console.log('Positive');
+    } else if (num < 0) {
+        console.log('Negative');
+    } else {
+        console.log('Zero');
+    }
+}
+
+// Example usage:
+checkNumber(5);    // Output: Positive
+checkNumber(-3);   // Output: Negative
+checkNumber(0);    // Output: Zero
+
+
+// Question 3: Loops
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+
